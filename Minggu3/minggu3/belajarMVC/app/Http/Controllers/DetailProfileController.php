72c9ad6387d50e4c6f100f456a9756447293d621
IvanoWoii <rezaivano123@gmail.com>
2023-04-04 08:52:29 +0700

    commit minggu ke 3

---
+++ Minggu3/minggu3/belajarMVC/app/Http/Controllers/DetailProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class DetailProfileController extends Controller
+{
+    //
+}
